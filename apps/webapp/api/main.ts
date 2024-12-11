5bd20c3a8402fab443621210834f16c3286575e2
Anil <adhurjaty@gmail.com>
2024-12-11 10:08:42 -0800

    Add serve npm command and update api to serve frontend routes (#13)

--- apps/webapp/api/main.ts
+++ apps/webapp/api/main.ts
@@ -3,6 +3,8 @@ import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
 import { poweredBy } from "@hono/hono/powered-by";
 import { extract } from '@extractus/article-extractor';
+import { serveStatic } from "@hono/hono/serve-static";
+import { trimTrailingSlash } from '@hono/hono/trailing-slash'
 
 const app = new Hono();
 
@@ -18,11 +20,14 @@ app.use(
     credentials: true,
   })
 );
-app.get("/", (c: Context) => {
+
+app.use(trimTrailingSlash());
+
+app.get("/api", (c: Context) => {
   return c.text("Hello Deno!");
 });
 
-app.get("/parsedArticle", async (c: Context) => {
+app.get("/api/parsedArticle", async (c: Context) => {
   const url = c.req.query("url");
   if (!url) {
     c.status(400);
@@ -50,6 +55,25 @@ v1Api.post("/headline", async (c: Context) => {
   return c.json({ transformedText });
 });
 
-app.route("/v1", v1Api);
+app.route("/api/v1", v1Api);
+
+app.use("/api/*", async (c: Context) => {
+  c.status(404);
+  return c.json({ error: "Not found" });
+});
+
+// all non-api routes are served from the dist folder where
+// the Vite build output is located (react frontend)
+app.use("*", serveStatic({
+  root: "./dist",
+  getContent: async (path) => {
+    console.log("Reading file", path);
+    try {
+      return await Deno.readFile(path);
+    } catch {
+      return await Deno.readFile("dist/index.html");
+    }
+  }
+}));
 
 Deno.serve(app.fetch);
