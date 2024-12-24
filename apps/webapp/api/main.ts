25bf3e1b20677ebeb1001cf434d401bc2f62ea02
Anil <adhurjaty@gmail.com>
2024-12-23 18:08:45 -0800

    Configure database connection local and prod (#16)
    * Set up local database connection with docker-compose
    
    * Set up prod db connection

--- apps/webapp/api/main.ts
+++ apps/webapp/api/main.ts
@@ -5,8 +5,29 @@ import { poweredBy } from "@hono/hono/powered-by";
 import { extract } from '@extractus/article-extractor';
 import { serveStatic } from "@hono/hono/serve-static";
 import { trimTrailingSlash } from '@hono/hono/trailing-slash'
+import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 const app = new Hono();
+
+const dbClient = new Client({
+  user: Deno.env.get("POSTGRES_USER"),
+  database: Deno.env.get("POSTGRES_DB"),
+  hostname: Deno.env.get("POSTGRES_HOST"),
+  port: 5432,
+  password: Deno.env.get("POSTGRES_PASSWORD"),
+});
+
+app.use("/api/*", async (_, next) => {
+  try {
+    await dbClient.connect();
+    await next();
+  } catch (e) {
+    console.error("Error connecting to database", e);
+    throw e;
+  } finally {
+    await dbClient.end();
+  }
+});
 
 app.use("*", logger(), poweredBy());
 app.use(
@@ -35,6 +56,11 @@ app.get("/api/parsedArticle", async (c: Context) => {
   }
   const parsed = await extract(url);
   return c.json(parsed);
+});
+
+app.get("/api/db-test", async (c: Context) => {
+  const result = await dbClient.queryArray("SELECT * FROM information_schema.tables");
+  return c.json(result.rows);
 });
 
 const v1Api = new Hono();
