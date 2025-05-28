2c56f21579a1e16a0bb0a709b374d69f6013063e
James Harris <computersherpa@gmail.com>
2025-05-28 18:01:00 -0400

    Tabs to spaces

--- apps/webapp/src/components/Page.tsx
+++ apps/webapp/src/components/Page.tsx
@@ -1,30 +1,30 @@
 export default function Page() {
-	return (
-		<>
-			<div className="hero-bg">
-				<div className="flex flex-col items-center hero">
-					<div className="hero-text">Repair the Headlines.</div>
-					<div className="hero-text">Rebuild the Narrative.</div>
-					<div className="hero-button">Get Started</div>
-				</div>
-			</div>
-			<div className="flex flex-responsive justify-between text-lg small-width">
-				<div className="column">
-					<div>Trending Replacements</div>
-					<ul className="my-5 pl-5 list-disc">
-						<li>New York Times: ...</li>
-						<li>Fox News: ...</li>
-					</ul>
-				</div>
-				<div className="column">
-					<div>Recently Adjudicated</div>
-					<ul className="my-5 pl-5 list-disc">
-						<li>CNN: ...</li>
-						<li>BBC: ...</li>
-					</ul>
-				</div>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className="hero-bg">
+        <div className="flex flex-col items-center hero">
+          <div className="hero-text">Repair the Headlines.</div>
+          <div className="hero-text">Rebuild the Narrative.</div>
+          <div className="hero-button">Get Started</div>
+        </div>
+      </div>
+      <div className="flex flex-responsive justify-between text-lg small-width">
+        <div className="column">
+          <div>Trending Replacements</div>
+          <ul className="my-5 pl-5 list-disc">
+            <li>New York Times: ...</li>
+            <li>Fox News: ...</li>
+          </ul>
+        </div>
+        <div className="column">
+          <div>Recently Adjudicated</div>
+          <ul className="my-5 pl-5 list-disc">
+            <li>CNN: ...</li>
+            <li>BBC: ...</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
 }
 
