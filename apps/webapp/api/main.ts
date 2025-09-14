0c56c09a26fce2fc4d9f8be7dee582a5c2cbb76c
gholtorf <gregory.holtorf@gmail.com>
2025-09-14 15:51:45 -0500

    Add article extracting library and setup demo (#12)
    # Conflicts fixed:
    #	apps/webapp/api/main.ts
    #	apps/webapp/deno.lock
    #	apps/webapp/src/App.tsx
    #	apps/webapp/src/backend/api.ts

--- apps/webapp/api/main.ts
+++ apps/webapp/api/main.ts
@@ -1,85 +1,55 @@
-import { Context, Hono, MiddlewareHandler } from "@hono/hono";
+import { Context, Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
 import { poweredBy } from "@hono/hono/powered-by";
-import { serveStatic } from "@hono/hono/serve-static";
-import { trimTrailingSlash } from '@hono/hono/trailing-slash'
-import { createMiddleware } from '@hono/hono/factory'
-import BasicDbRepo from "./basicDbRepo.ts";
-import { 
-  sessionMiddleware, 
-  CookieStore 
-} from 'jsr:@jcs224/hono-sessions'
-import sessions from "./sessions.ts";
-import test from "./test.ts";
-import replacements from "./replacements.ts";
-import transformations from "./transformations.ts";
-import v1Api from "./v1Api.ts";
-import { Env } from "./env.ts";
+import { extract } from '@extractus/article-extractor';
 
-const corsPolicy = cors({
-  origin: ["*"], // TODO: Change this to trust-assembly.org, or whatever URL we're using frontend URL
-  allowMethods: ["POST", "GET", "OPTIONS"],
-  allowHeaders: ["Content-Type"],
-  exposeHeaders: ["Content-Length"],
-  maxAge: 600,
-  credentials: true,
-});
+const app = new Hono();
 
-const store = new CookieStore()
-const sessionMiddlewareHanlder = sessionMiddleware({
-  store,
-  encryptionKey: 'password_at_least_32_characters_long', // TODO: change this to a secure key
-  expireAfterSeconds: 900, // Expire session after 15 minutes of inactivity
-  cookieOptions: {
-    sameSite: 'Lax',
-    path: '/', // Required for this library to work properly
-    httpOnly: true, // Recommended to avoid XSS attacks
-  },
-}) as unknown as MiddlewareHandler<Env, string, {}>;
-
-const dbMiddleware = createMiddleware(async (c, next) => {
-  using db = await BasicDbRepo.create();
-  c.set('db', db);
-  await next()
-});
-
-const apiApp = new Hono<Env>()
-  .use('*', sessionMiddlewareHanlder)
-  .use("*", dbMiddleware)
-  .route("/", sessions)
-  .route("/", transformations)
-  .route("/test", test)
-  .route("/replacements", replacements)
-  .route("/v1", v1Api)
-  .get("/", (c) => {
-    return c.text("Hello Deno!");
+app.use("*", logger(), poweredBy());
+app.use(
+  "*",
+  cors({
+    origin: ["*"], // TODO: Change this to trust-assembly.org, or whatever URL we're using frontend URL
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    allowHeaders: ["Content-Type"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
   })
-  .use("*", async (c: Context) => {
-    c.status(404);
-    return c.json({ error: "Not found" });
+);
+app.get("/", (c: Context) => {
+  return c.text("Hello Deno!");
+});
+
+app.get("/parsedArticle", async (c: Context) => {
+  const url = c.req.query("url");
+  if (!url) {
+    c.status(400);
+    return c.json({ error: "URL is required" });
+  }
+  const parsed = await extract(url);
+  return c.json(parsed);
+});
+
+const v1Api = new Hono();
+
+const transformHeadline = (headline: string): Promise<string> => {
+  // TODO: Implement the logic to transform the headline using OpenAI
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(headline.toUpperCase());
+    }, 100); // Simulating some asynchronous operation
   });
+};
 
-const rootApp = new Hono<Env>()
-  .use("*", logger(), poweredBy())
-  .use(
-    "*",
-    corsPolicy,
-  )
-  .use(trimTrailingSlash())
-  .route("/api", apiApp)
-  .use("*", serveStatic({
-    // all non-api routes are served from the dist folder where
-    // the Vite build output is located (react frontend)
-    root: "./dist",
-    getContent: async (path) => {
-      console.log("Reading file", path);
-      try {
-        return await Deno.readFile(path);
-      } catch {
-        return await Deno.readFile("dist/index.html");
-      }
-    }
-  }));
+v1Api.post("/headline", async (c: Context) => {
+  const { headline } = await c.req.json();
 
-Deno.serve(rootApp.fetch);
+  const transformedText = await transformHeadline(headline);
+  return c.json({ transformedText });
+});
+
+app.route("/v1", v1Api);
+
+Deno.serve(app.fetch);
--- apps/webapp/src/App.tsx
+++ apps/webapp/src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Index from "./Index";
 import Hello from "./Hello";
 import './App.css'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import QueryBoundaryProvider from "./contexts/QueryBoundaryProvider";
 import ParsedArticle from "./ParsedArticle";
-import SessionProvider from "./contexts/SessionProvider";
-import Index from "./Index";
-import Replacements from "./Replacements";
 
 function App() {
   const queryClient = new QueryClient({
@@ -20,16 +18,13 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <QueryBoundaryProvider>
-        <SessionProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/hello" element={<Hello />} />
-              <Route path="/parsedArticle" element={<ParsedArticle />} />
-              <Route path="/replacements" element={<Replacements />} />
-            </Routes>
-          </BrowserRouter>
-        </SessionProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/hello" element={<Hello />} />
+            <Route path="/parsedArticle" element={<ParsedArticle />} />
+          </Routes>
+        </BrowserRouter>
       </QueryBoundaryProvider>
     </QueryClientProvider>
   )
